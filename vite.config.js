1ce9aa3fc8ead1a02c761171f42c9cd1a2666f89
lionelbalaji <balaji2405jeeva@gmail.com>
2024-05-30 18:48:48 +0530

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/softdrinks-app/',
-  plugins: [react()]
- 
+  
+  plugins: [react()],
+  base:'/softdrinks-app/'
 })
